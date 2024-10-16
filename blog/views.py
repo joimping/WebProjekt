75e901a0cd52d1ad08a874dc862f9dccd2b19703
Jo Imping <jo.imping@dhbw-engineering.de>
2024-10-16 22:40:03 +0200

    7 commit / added xml extraction feature

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog(reqest):
-    
 
+   # XML-Datei parsen
+   tree = ET.parse('xml/data.xml')
+   root = tree.getroot()
 
-    if reqest.method == 'POST':
-       print('Empfangen' , 'Name: ',reqest.POST['name'],'Text: ' , reqest.POST['text']) 
-    return render(reqest, 'blogs.html')
+   # Daten extrahieren
+   daten = []
+   for auto in root:
+      auto_daten = {}
+      auto_daten['marke'] = auto.find('Automarke').text
+      auto_daten['bezeichnung'] = auto.find('Autobezeichnung').text
+      technische_details = auto.find('Technische_Details')
+      auto_daten['leistung'] = technische_details.find('Leistung').text
+      auto_daten['drehmoment'] = technische_details.find('max_Drehmomemt').text
+      daten.append(auto_daten)
+
+   # Daten ausgeben
+   print(daten)
+
+   if reqest.method == 'POST':
+      print('Empfangen' , 'Name: ',reqest.POST['name'],'Text: ' , reqest.POST['text']) 
+   return render(reqest, 'blogs.html')
