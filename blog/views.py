34410589eba200f4b6dae13d6f97ebe3bc969406
Jo Imping <jo.imping@dhbw-engineering.de>
2024-10-16 21:40:05 +0200

    6 commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
+import xml.etree.ElementTree as ET
 from django.shortcuts import render
 
 # Create your views here.
 
 def blog(reqest):
+    
+
+
     if reqest.method == 'POST':
-       print('Empfangen' , 'Name: ',reqest.POST['name'],'Text: ' , reqest.POST['text']) #Hier XML Anbindung
+       print('Empfangen' , 'Name: ',reqest.POST['name'],'Text: ' , reqest.POST['text']) 
     return render(reqest, 'blogs.html')
